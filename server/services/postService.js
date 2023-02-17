36487794faf21688a5b784668b1b31d8dc0124a9
Thomas Finneman <thomasfinneman94@gmail.com>
2023-02-17 17:54:35 +0100

    del5

--- server/services/postService.js
+++ server/services/postService.js
@@ -16,165 +16,134 @@ const constraints = {
     }
 };
 
-async function getByTag(tagId) {
-  try {
-    const tag = await db.tag.findOne({ where: { id: tagId } });
-    const allPosts = await tag.getPosts({ include: [db.user, db.tag] });
-    /* Om allt blev bra, returnera allPosts */
-    return createResponseSuccess(allPosts.map((post) => _formatPost(post)));
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
-}
+async function getById(id){
+    try {
+        const post = await db.post.findOne({
+            where: {id},
+            include: [
+                db.user,
+                db.tag, 
+                {
+                    model: db.comment,
+                    include: [db.user]
+                }
+            ]
+        });
 
-async function getByAuthor(userId) {
-  try {
-    const user = await db.user.findOne({ where: { id: userId } });
-    const allPosts = await user.getPosts({ include: [db.user, db.tag] });
-    /* Om allt blev bra, returnera allPosts */
-    return createResponseSuccess(allPosts.map((post) => _formatPost(post)));
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
+        return createResponseSuccess(_formatPost(post));
+    } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
 }
+async function getAll(){
+    try {
+        const allPosts = await db.post.findAll({include: [db.user, db.tag]});
 
-async function getById(id) {
-  try {
-    const post = await db.post.findOne({
-      where: { id },
-      include: [
-        db.user,
-        db.tag,
-        {
-          model: db.comment,
-          include: [db.user]
+        return createResponseSuccess(allPosts);
+    } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
+    /*kontrollera allPosts, om något blev fel retunera felmeddelande, om allt blev bra retunera allPosts*/
+}
+async function addComment(id, comment){
+    if(!id){
+        return createResponseError(422, "id är obligatoriskt");
+    }
+    try{
+        comment.postId = id;
+        const newComment = await db.comment.create(comment);
+        return createResponseSuccess(newComment);
+    } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
+}
+async function create(post){
+    const invalidData = validate(post, constraints);
+    if(invalidData){
+        return createResponseError(422, invalidData);
+    } else{
+        try{
+            const newPost = await db.post.create(post);
+            return createResponseSuccess(newPost);
+        } catch (error) {
+            return createResponseError(error.status, error.message);
         }
-      ]
-    });
-    /* Om allt blev bra, returnera post */
-    return createResponseSuccess(_formatPost(post));
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
-}
-
-async function getAll() {
-  try {
-    const allPosts = await db.post.findAll({ include: [db.user, db.tag] });
-    /* Om allt blev bra, returnera allPosts */
-    return createResponseSuccess(allPosts.map((post) => _formatPost(post)));
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
-}
-
-async function addComment(id, comment) {
-  if (!id) {
-    return createResponseError(422, 'Id är obligatoriskt');
-  }
-  try {
-    comment.postId = id;
-    const newComment = await db.comment.create(comment);
-    return createResponseSuccess(newComment);
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
-}
-
-async function create(post) {
-  const invalidData = validate(post, constraints);
-  if (invalidData) {
-    return createResponseError(422, invalidData);
-  }
-  try {
-    const newPost = await db.post.create(post);
-    //post tags är en array av namn
-    //lägg till eventuella taggar
-    await _addTagToPost(newPost, post.tags);
-
-    return createResponseSuccess(newPost);
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
-}
-
-async function update(post, id) {
-  const invalidData = validate(post, constraints);
-  if (!id) {
-    return createResponseError(422, 'Id är obligatoriskt');
-  }
-  if (invalidData) {
-    return createResponseError(422, invalidData);
-  }
-  try {
-    const existingPost = await db.post.findOne({ where: { id } });
-    if (!existingPost) {
-      return createResponseError(404, 'Hittade inget inlägg att uppdatera.');
     }
-    await _addTagToPost(existingPost, post.tags);
-    await db.post.update(post, {
-      where: { id }
-    });
-    return createResponseMessage(200, 'Inlägget uppdaterades.');
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
 }
-async function destroy(id) {
-  if (!id) {
-    return createResponseError(422, 'Id är obligatoriskt');
-  }
-  try {
-    await db.post.destroy({
-      where: { id }
-    });
-    return createResponseMessage(200, 'Inlägget raderades.');
-  } catch (error) {
-    return createResponseError(error.status, error.message);
-  }
+async function update(post, id){
+    const invalidData = validate(post, constraints);
+    if(!id){
+        return createResponseError(422, "id är obligatoriskt");
+    }
+    if(invalidData || !id){
+        return createResponseError(422, invalidData);
+    }
+
+    try {
+        await db.post.update(post, {
+            where: {id}
+        })
+        .then(result =>{
+            res.send(result);
+        });
+        return createResponseMessage(200, "inlägget uppdaterades")       
+    } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
+}
+async function destroy(id){
+    if(!id){
+        return createResponseError(422, "id är obligatoriskt");
+    }
+    try {
+        await db.post.destroy({
+            where: {id}
+        });
+        return createResponseMessage(200, "inlägget raderades") 
+    } catch (error) {
+        return createResponseError(error.status, error.message);
+    }
 }
 
-function _formatPost(post) {
+function _formatPost(post){
     const cleanPost = {
-      id: post.id,
-      title: post.title,
-      body: post.body,
-      imageUrl: post.imageUrl,
-      createdAt: post.createdAt,
-      updatedAt: post.updatedAt,
-      author: {
-        id: post.user.id,
-        username: post.user.username,
-        email: post.user.email,
-        firstName: post.user.firstName,
-        lastName: post.user.lastName,
-        imageUrl: post.user.imageUrl
-      },
-      tags: []
+        id: post.id,
+        title: post.title,
+        body: post.body,
+        imageUrl: post.imageUrl,
+        createdAt: post.createdAt,
+        updatetAt: post.updatetAt,
+        author: {
+            id: post.user.id,
+            username: post.user.username,
+            email: post.user.email,
+            firstName: post.user.firstName,
+            lastName: post.user.lastName,
+            imageUrl: post.user.imageUrl
+        },
+        tags: []
     };
-  
+
     if (post.comments) {
-      cleanPost.comments = [];
-  
-      post.comments.map((comment) => {
-        return (cleanPost.comments = [
-          {
-            title: comment.title,
-            body: comment.body,
-            author: comment.user.username,
-            createdAt: comment.createdAt
-          },
-          ...cleanPost.comments
-        ]);
-      });
+        cleanPost.comments = [];
+        post.comments.map((comment) => {
+            return (cleanPost.comments = [
+                {
+                    title: comment.title,
+                    body: comment.body,
+                    author: comment.user.username,
+                    createdAt: comment.createdAt
+                },
+                ...cleanPost.comments
+            ]);
+        });
     }
-  
-    if (post.tags) {
-      post.tags.map((tag) => {
-        return (cleanPost.tags = [tag.name, ...cleanPost.tags]);
-      });
-      return cleanPost;
+    if(post.tags){
+        post.tags.map((tag) =>{
+            return (cleanPost.tags = [tag.name, ...cleanPost.tags]);
+        });
+        return cleanPost;
     }
-  }
+}
 
 module.exports = {getById, getAll, addComment, create, update, destroy};
