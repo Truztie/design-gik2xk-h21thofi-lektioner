dd1200f143d9a0b3bbb41f5295dfb9ec290a24f6
Thomas Finneman <thomasfinneman94@gmail.com>
2023-03-02 17:47:03 +0100

    lek 6

--- client/src/models/PostModel.js
+++ client/src/models/PostModel.js
@@ -59,3 +59,15 @@ export async function remove(id){
         return {};
     }
 }
+
+export async function addComment(id, comment){
+    const result = await api.post(`/posts/${id}/addComment`, comment);
+
+    if(result.status === 200){
+        return result.data;
+    }else{
+        console.log(result.status);
+        console.log(result.data);
+        return {};
+    }
+}
--- client/src/views/PostDetail.js
+++ client/src/views/PostDetail.js
@@ -2,7 +2,8 @@ import { Button} from '@mui/material';
 import { useParams, Link} from 'react-router-dom';
 import { useState, useEffect} from 'react';
 import PostItemLarge from '../components/PostItemLarge';
-import { getOne } from '../models/PostModel';
+import { addComment, getOne } from '../models/PostModel';
+import CommentForm from '../components/CommentForm';
 
 function PostDetail() {
   //Ta emot ett id för att hämta ett inlägg posts/:id
@@ -15,13 +16,19 @@ function PostDetail() {
   useEffect(() => {
     getOne(postId).then((post) => setPost(post))
   }, [postId]);
+
+  function onCommentAdd(comment){
+    addComment(postId, comment).then((post) => setPost(post))
+  }
+
   return (
     <>
       <PostItemLarge post={post} />
+      <CommentForm onSave={onCommentAdd}></CommentForm>
       <div>
         {post.comments &&
           post.comments.map((comment, i) => (
-            <p key={`commentId_${i}`}>{comment.title}</p>
+            <p key={`commentId_${i}`}>{comment.title}<br/> {comment.body}</p>
           ))}
       </div>
       <Link to={`/posts/${postId}/edit`}>
